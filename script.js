107839cb146c7b4ac3923e09a5ae76444dfa9b21
Marco-Rocco <marcorocco28@icloud.com>
2025-03-13 15:42:18 +0100

    feat: show emails in page

--- script.js
+++ script.js
@@ -5,29 +5,25 @@ const randomMail = 'https://flynn.boolean.careers/exercises/api/random/mail'
 let emailList = document.getElementById('listcontainer')
 console.log(emailList)
 
+// creo una variabile al di fuori del ciclo (da riassegnare dopo) e poi la CONCATENO, così invece di riassegnarsi 10 volte, se ne aggunge uno nuovo
+// così da mostrare 10 mail invece che 1 sola. 
+let result = ''
+
 for (let i = 1; i <= 10; i++ ){
     axios.get(randomMail) 
         .then((response) => {
             let showEmail = response.data.response;
-
-
-            emailList.innerHTML = 
-            `
-            <li>${showEmail}</li>
-            <li>${showEmail}</li>
-            <li>${showEmail}</li>
-            <li>${showEmail}</li>
-            <li>${showEmail}</li>
-            <li>${showEmail}</li>
-            <li>${showEmail}</li>
-            <li>${showEmail}</li>
-            <li>${showEmail}</li>
-
-            `
-
-
-
+            
             console.log(showEmail)
+        
+            // qui riassegno la variabile usando il backtick, e poi lo aggiungo all'inner html
+            result += `<li>${showEmail}</li> `
+            emailList.innerHTML = result
+            
+
+
+
+            
         })
         .catch((error => {
             console.error('ERRORE')
