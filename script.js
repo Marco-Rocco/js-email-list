dfe72a90cf016dd50485bdf7eb05eba46f07d56d
Marco-Rocco <marcorocco28@icloud.com>
2025-03-13 17:32:51 +0100

    feat: crea un'evento che invoca una funzione creata per generare le mail

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ const randomMail = 'https://flynn.boolean.careers/exercises/api/random/mail'
 let emailList = document.getElementById('listcontainer')
 console.log(emailList)
 
+
+//invoca la funzione
+generateMails()
+
+
+//crea una funzione che aziona il ciclo e genera la mail
+function generateMails(){
 // creo una variabile al di fuori del ciclo (da riassegnare dopo) e poi la CONCATENO, così invece di riassegnarsi 10 volte, se ne aggunge uno nuovo
 // così da mostrare 10 mail invece che 1 sola. 
 let result = ''
@@ -25,17 +32,17 @@ for (let i = 1; i <= 10; i++ ){
         .catch((error => {
             console.error('ERRORE')
         }))
+} 
 }
-
 
 //////////
 
 // collego il bottone
 let fetchButton = document.getElementById('fetch10');
 
-// crea l'evento al click
-fetchButton.addEventListener('click', function(){
+// crea l'evento al click che richiama la funziona nuovamente
+fetchButton.addEventListener('click', function(event){
+    event.preventDefault
     console.log('click')
-    fetch(randomMail)
-    .then(response)
+    generateMails()
 })
