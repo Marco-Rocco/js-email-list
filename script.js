d5fcc56c757d643f00245200aef9338c8b8cc8b6
Marco-Rocco <marcorocco28@icloud.com>
2025-03-13 15:12:05 +0100

    feat: show email in page

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
+//connetto api
 const randomMail = 'https://flynn.boolean.careers/exercises/api/random/mail'
+
+//connetto ul
+let emailList = document.getElementById('listcontainer')
+console.log(emailList)
 
 for (let i = 1; i <= 10; i++ ){
     axios.get(randomMail) 
         .then((response) => {
-            console.log(response)
+            let showEmail = response.data.response;
+
+
+            emailList.innerHTML = 
+            `
+            <li>${showEmail}</li>
+            <li>${showEmail}</li>
+            <li>${showEmail}</li>
+            <li>${showEmail}</li>
+            <li>${showEmail}</li>
+            <li>${showEmail}</li>
+            <li>${showEmail}</li>
+            <li>${showEmail}</li>
+            <li>${showEmail}</li>
+
+            `
+
+
+
+            console.log(showEmail)
         })
         .catch((error => {
-            console.error('bashtardo')
+            console.error('ERRORE')
         }))
 }
+
+
+
+
